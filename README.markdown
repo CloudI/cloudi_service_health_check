19e5de3c3e2afeb1362d01d0aa03e8eaf4358d9f
Michael Truog <mjtruog@protonmail.com>
2022-08-26 22:14:22 -0700

    Add cloudi_service_health_check for remote host health checks.

---
+++ README.markdown
@@ -0,0 +1,7 @@
+[![Build Status](https://secure.travis-ci.org/CloudI/cloudi_service_health_check.png?branch=master)](http://travis-ci.org/CloudI/cloudi_service_health_check)
+[![hex.pm version](https://img.shields.io/hexpm/v/cloudi_service_health_check.svg)](https://hex.pm/packages/cloudi_service_health_check)
+
+If you are looking to see if you need this repository, please refer to the [`cloudi_core` repository](https://github.com/CloudI/cloudi_core#about).
+
+If you are looking for CloudI service integration documentation, [go here](https://github.com/CloudI/CloudI#integration).
+
---
+++ mix.exs
@@ -0,0 +1,47 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
+defmodule CloudIServiceHealthCheck do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_health_check,
+     version: "2.0.5",
+     language: :erlang,
+     erlc_options: [
+       :debug_info,
+       :warnings_as_errors,
+       :strict_validation,
+       :warn_bif_clash,
+       :warn_deprecated_function,
+       :warn_export_all,
+       :warn_export_vars,
+       :warn_exported_vars,
+       :warn_obsolete_guard,
+       :warn_shadow_vars,
+       :warn_unused_import,
+       :warn_unused_function,
+       :warn_unused_record,
+       :warn_unused_vars],
+     description: description(),
+     package: package(),
+     deps: deps()]
+  end
+
+  defp deps do
+    [{:cloudi_core, ">= 2.0.5"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework Health Check CloudI Service"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown LICENSE),
+     maintainers: ["Michael Truog"],
+     licenses: ["MIT"],
+     links: %{"Website" => "https://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_health_check"}]
+   end
+end
