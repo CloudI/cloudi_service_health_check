077fd1f3e046ab0215d5952e8bdaf5b6b0d1017a
Michael Truog <mjtruog@protonmail.com>
2023-06-20 20:03:53 -0700

    Version 2.0.6.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceHealthCheck do
 
   def project do
     [app: :cloudi_service_health_check,
-     version: "2.0.5",
+     version: "2.0.6",
      language: :erlang,
      erlc_options: [
        :deterministic,
@@ -21,7 +21,7 @@ defmodule CloudIServiceHealthCheck do
   end
 
   defp deps do
-    [{:cloudi_core, ">= 2.0.5"}]
+    [{:cloudi_core, ">= 2.0.6"}]
   end
 
   defp description do
